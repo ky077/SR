c26495134e573b133b9c21865526f3a9282455fe
ky077 <kylin077@gmail.com>
2025-11-28 15:24:11 +0800

    20251128 SR TEACHER

--- SRteacher/Content/js/doubleScorollbar.js
+++ SRteacher/Content/js/doubleScorollbar.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
 	//多個Modal，使最新開啟的modal顯示在最上層 
 	//深思海数_willschang http://www.jianshu.com/p/0f05e628f87b
 	$(document).on('show.bs.modal', '.modal', function (event) {
-		var zIndex = 1040 + (10 * $('.modal:visible').length);
+		var zIndex = 1045 + (10 * $('.modal:visible').length);
 		$(this).css('z-index', zIndex);
 		 setTimeout(function() {
 			 $('.modal-backdrop').not('.modal-stack').css('z-index', zIndex - 1).addClass('modal-stack');
